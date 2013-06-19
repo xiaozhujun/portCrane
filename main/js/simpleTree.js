7e341f6382a770b0fee4fb43070b670ce69cb600
TommyDin <dt22797779@gmail.com>
2013-06-19 16:25:04 +0800

    done!

---
+++ main/js/simpleTree.js
@@ -0,0 +1,18 @@
+function simpleTree(obj) {
+	$(".itemTitle").addClass("titleHide");
+	$(".itemContent").addClass("contentHide");
+	$(".itemTitle").click(function(){
+		if ($(this).hasClass("titleShow")) {
+			$(this).removeClass("titleShow").addClass("titleHide");
+			$(this).next().removeClass("contentShow").addClass("contentHide");
+		} else if ($(this).hasClass("titleHide")) {
+			$(this).removeClass("titleHide").addClass("titleShow");
+			$(this).next().removeClass("contentHide").addClass("contentShow");
+		};
+	});
+	$(".bullet").click(function(){
+		$(".menuSelected").removeClass("menuSelected");
+		$(this).addClass("menuSelected");
+	});
+}
+ 
