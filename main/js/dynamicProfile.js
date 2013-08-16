533a9660e75c0313c77afcb8dfc2fc5d90e5e24a
TommyDin <dt22797779@gmail.com>
2013-08-16 11:10:54 +0800

    动态曲线图

--- main/js/dynamicProfile.js
+++ main/js/dynamicProfile.js
@@ -1,28 +1,25 @@
-	function GraphDrawer (target,data) {
-		this.target = target;
-		this.data = data;
-		this.drawGraph = function(){
-		  RGraph.Clear(document.getElementById(target));
-		  RGraph.ObjectRegistry.Clear();
-		  var line = new RGraph.Line(target, data)
-		      .Set('colors', ['green'])
-		      .Set('linewidth', 1)
-		      .Set('filled', true)
-		      .Set('fillstyle', 'rgba(128,255,128,0.5)')
-		      .Set('ymax', 60)
-		      .Set('numxticks', 5)
-		      .Set('labels', ['Now','10s','20s','30s','40s','50s','60s'])
-		      .Draw();
-		  r    = RGraph.random(20,55);
-		  data = [r].concat(data);
-		  data.pop();
-		  // this.timer = setInterval(this.drawGraph, 1000);
-	  };
-	  // this.stopDrawing = function(){
-	  // 	clearInterval(this.timer);
-	  // };
-	}
 
+			function GraphDrawer (target,data) {
+        this.drawGraph = function(){
+          RGraph.Clear(document.getElementById(target));
+          RGraph.ObjectRegistry.Clear();
+          var line = new RGraph.Line(target, data)
+              .Set('colors', ['green'])
+              .Set('linewidth', 1)
+              .Set('filled', true)
+              .Set('fillstyle', 'rgba(128,255,128,0.5)')
+              .Set('ymax', 200)
+              .Set('numxticks', 5)
+              .Set('labels', ['Now','10s','20s','30s','40s','50s','60s'])
+              .Draw();
+          data = [sensorValue].concat(data);
+          data.pop();
+          this.timer = setInterval(this.drawGraph, 500);
+          };
+          this.stopDrawing = function(){
+            clearInterval(this.timer);
+          };
+        }
 
 	// var target;
 	// var data;
