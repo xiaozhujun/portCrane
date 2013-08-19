c320523e825b2ebfa737138279bcffc627941fa6
TommyDin <dt22797779@gmail.com>
2013-08-19 14:46:45 +0800

    ..

--- main/js/dynamicProfile.js
+++ main/js/dynamicProfile.js
@@ -1,5 +1,35 @@
 
-			function GraphDrawer (target,data) {
+			function RealGraphDrawer1 (target,dataArr1) {
+        this.drawGraph = function(){
+          RGraph.Clear(document.getElementById(target));
+          RGraph.ObjectRegistry.Clear();
+          var line = new RGraph.Line(target, dataArr1)
+              .Set('colors', ['green'])
+              .Set('linewidth', 1)
+              .Set('filled', true)
+              .Set('fillstyle', 'rgba(128,255,128,0.5)')
+              .Set('ymax', 60)
+              .Set('numxticks', 5)
+              .Set('labels', ['Now','10s','20s','30s','40s','50s','60s'])
+              .Draw();
+          if(sensorValue1.length==0){
+          	sensorValue1 = RGraph.random(0,60);
+            dataArr1 = [sensorValue1].concat(dataArr1);
+          } else{
+            dataArr1 = sensorValue1.concat(dataArr1);
+            for (var i = 0; i < popLen1; i++) {
+              dataArr1.pop();
+            };
+          }
+          this.timer = setInterval(this.drawGraph, 1000);
+          };
+          this.stopDrawing = function(){
+            clearInterval(this.timer);
+          };
+        }
+
+
+      function GraphDrawer (target,data) {
         this.drawGraph = function(){
           RGraph.Clear(document.getElementById(target));
           RGraph.ObjectRegistry.Clear();
@@ -8,15 +38,13 @@
               .Set('linewidth', 1)
               .Set('filled', true)
               .Set('fillstyle', 'rgba(128,255,128,0.5)')
-              .Set('ymax', 200)
+              .Set('ymax', 60)
               .Set('numxticks', 5)
               .Set('labels', ['Now','10s','20s','30s','40s','50s','60s'])
               .Draw();
-          if(typeof(sensorValue)=="undefined"){
-          	sensorValue = RGraph.random(0,200);
-          }
-          data = [sensorValue].concat(data);
-          data.pop();
+          var r = RGraph.random(0,60);
+          data = [r].concat(data);
+          
           this.timer = setInterval(this.drawGraph, 1000);
           };
           this.stopDrawing = function(){
